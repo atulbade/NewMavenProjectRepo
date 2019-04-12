28c2ccf7d191fc3e760cb44445597e0a08b76dc1
unknown <badeatul1@gmail.com>
2019-04-12 11:13:47 +0530

    Transfer to github

---
+++ src/test/java/MavenPackage/FirstMavenclass_Test.java
@@ -0,0 +1,13 @@
+package MavenPackage;
+
+import org.testng.annotations.Test;
+
+public class FirstMavenclass_Test {
+	
+	@Test
+	public void method()
+	{
+		System.out.println("Hello World!");
+	}
+
+}
